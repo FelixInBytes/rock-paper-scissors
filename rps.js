1848b0d30924e197fd0cacbffa02538efb4a2452
Felix Schumacher <felixschumacher@ymail.com>
2023-08-12 19:42:59 +0200

    Added functions
    - added get computerchoice() to calculate r,p or s
    - added get randomint() for a random int for computerchoice()

---
+++ rps.js
@@ -0,0 +1,14 @@
+
+//get random int counted by max parameter
+// max = 3 => output = 0,1,2
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+getComputerChoice(){
+    var rk = "Rock";
+    var pp = "Paper";
+    var si = "Scissors";
+
+
+}
