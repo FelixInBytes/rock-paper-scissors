b617579b8b07ee7913c023ceaa26df952e1b54a3
Felix Schumacher <felixschumacher@ymail.com>
2023-08-20 19:50:49 +0200

    added EventListener
    - takes buttons from page
    - puts their value prop into a var
    - playGame now uses interactive button player input

--- rps.js
+++ rps.js
@@ -1,67 +1,69 @@
-
 //get random int counted by max parameter
 // max = 3 => output = 0,1,2
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 // get a r,p,s choice from the computer
-function getComputerChoice(){
+function getComputerChoice() {
+  const rk = "Rock";
+  const pp = "Paper";
+  const si = "Scissors";
+  var choice = getRandomInt(3);
 
-    const rk = "Rock";
-    const pp = "Paper";
-    const si = "Scissors";
-    var choice = getRandomInt(3);
-
-    switch (choice) {
-        case 0:
-            return rk;
-            break;
-        case 1:
-            return pp;
-            break;
-        case 2:
-            return si;
-            break;
-        default:
-            console.log("Sorry something went wrong!");
-    }
-
+  switch (choice) {
+    case 0:
+      return rk;
+      break;
+    case 1:
+      return pp;
+      break;
+    case 2:
+      return si;
+      break;
+    default:
+      console.log("Sorry something went wrong!");
+  }
 }
 
-function play(playerSelection, computerSelection) {
-    
+function playRounds() {
+  var rounds = Number(window.prompt("How many rounds do you want to play?"));
 
-    var psUpper = playerSelection.toUpperCase();
-    var csUpper = computerSelection.toUpperCase();
+  for (let index = 0; index < rounds; index++) {
+    var playersChoice = window.prompt("Rock, Paper or Scissors?");
+    var compChoice = getComputerChoice();
+
+    var end = play(playersChoice, compChoice);
+    console.log(end);
+  }
+}
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", playGame);
+
+  function playGame() {
+    var psUpper = (document.getElementsByClassName(".playerBTN").innerHTML =
+      this.value.toUpperCase());
+    var csUpper = getComputerChoice().toUpperCase();
+
+    console.log(psUpper);
+    console.log(csUpper);
 
     var win = "You Win! " + psUpper + " beats " + csUpper;
     var lose = "You Lose! " + csUpper + " beats " + psUpper;
 
-
     if (psUpper == "ROCK" && csUpper == "SCISSORS") {
-        return win;
-    }else if(psUpper == "SCISSORS" && csUpper == "PAPER"){
-        return win;
-    }else if(psUpper == "PAPER" && csUpper == "ROCK"){
-        return win;
-    }else if(psUpper == csUpper){
-        return "Tie!";
-    }else{
-        return lose;
+      console.log(win);
+    } else if (psUpper == "SCISSORS" && csUpper == "PAPER") {
+      console.log(win);
+    } else if (psUpper == "PAPER" && csUpper == "ROCK") {
+      console.log(win);
+    } else if (psUpper == csUpper) {
+      console.log("Tie!");
+    } else {
+      console.log(lose);
     }
-
-}
-
-function playRounds() {
-
-    var rounds = Integer(window.prompt("How many rounds do you want to play?"));
-
-    for (let index = 0; index < rounds; index++) {
-        var playersChoice =  window.prompt("Rock, Paper or Scissors?");
-        var compChoice = getComputerChoice();
-
-        play(playersChoice, compChoice);
-    }
-    
-}
+  }
+});
